7f9094de389cc13e4bb7cc247fee9ea4b3a3137f
Matthew Do <Matt2037@gmail.com>
2024-08-03 18:07:05 -0700

    chore: Serve React App with Flask

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS  # Import CORS
 import sqlite3
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='dist')
 CORS(app)  # Enable CORS for all routes
 
 def get_db_connection():
@@ -32,6 +32,15 @@ def get_pokemon_by_id(id):
 @app.route('/sprites/<path:filename>', methods=['GET'])
 def get_sprite(filename):
     return send_from_directory(os.path.join(app.root_path, 'static/sprites'), filename)
+
+# Serve React App
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
